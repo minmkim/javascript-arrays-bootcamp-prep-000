6740105192cd529606f9f64a48d3bb99eeea92ef
Min Kim <minmkim@gmail.com>
2018-07-07 00:53:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  newArray = [element ... array]
+  newArray = [element, ...array]
   return array
 }
 
