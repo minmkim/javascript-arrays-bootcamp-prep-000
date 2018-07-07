84a208f742ee167a20cea1ec053dc68f6c5a16be
Min Kim <minmkim@gmail.com>
2018-07-07 00:38:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,9 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   var newArray = addElementToBeginningOfArray(array, element)
   return newArray
 }
+
+function addElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
+function 
